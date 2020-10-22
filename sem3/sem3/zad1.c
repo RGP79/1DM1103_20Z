60b1a0e9e0f59e469830475fd2fb00333db87d01
RGP79 <odziemimeczek@gmail.com>
2020-10-22 16:42:26 +0200

    zadanie domowe1

---
+++ sem3/sem3/zad1.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+	float a1, a2;
+	a1 = atof(argv[1]);
+	a2 = a1 * a1;
+	printf("Wartość %1.0f do kwadratu to %1.0f .\n", a1, a2);
+
+
+		return 0;
+}
