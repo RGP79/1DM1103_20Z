2bfd41ffdcdb98facbd41715434b228c3f39f297
RGP79 <odziemimeczek@gmail.com>
2020-11-01 17:03:05 +0100

    zadanie 2 mnozenie macierzy

--- sem3/sem3/zad1.c
+++ sem3/sem3/zad1.c
@@ -1,4 +1,4 @@
-﻿#include<stdio.h>
+﻿/*#include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
 	float a1, a2;
 	a1 = atof(argv[1]);
 	a2 = a1 * a1;
-	printf("Wartość %1.0f do kwadratu to %1.0f .\n", a1, a2);
+	printf("Wartość %1.0f do kwadratu to %1.0f\n", a1, a2);
 
 
 		return 0;
-}
+}*/
---
+++ sem4/sem4/zad2.c
@@ -0,0 +1,93 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void macierz(FILE *fina, int wa, int ka, float taba[100][100])
+{
+	float la;
+
+	if (fina == NULL)
+	{
+		printf("Plik nie może zostać otwarty\n");
+	}
+	else
+	{
+		printf("macierz A o rozmiarze (%dx%d):\n", wa, ka);
+		for (int a = 0; a < wa; a++)
+		{
+			printf("[ ");
+			for (int b = 0; b < ka; b++)
+			{
+				fscanf(fina, "%f", &la);
+				taba[a][b] = la;
+				printf("%.1f ", taba[a][b]);
+			}
+			printf("]\n");
+		}
+	}
+}
+
+void wektor(FILE *finx, int wx, int kx, float tabx[100][100])
+{
+	float lx, w;
+	
+	if (finx == NULL)
+	{
+		printf("Plik nie może zostać otwarty\n");
+	}
+	else
+	{
+		printf("wektor x o rozmiarze (%dx%d):\n", wx, kx);
+		for (int a = 0; a < wx; a++)
+		{
+			printf("[ ");
+			for (int b = 0; b < kx; b++)
+			{
+				fscanf(finx, "%f", &lx);
+				tabx[a][b] = lx;
+				printf("%.1f ", tabx[a][b]);
+			}
+			printf("]\n");
+		}
+	}
+}
+
+float wymnoz(FILE* fina, FILE* finx, float taba[100][100], float tabx[100][100], int wa, int ka, int wx, int kx)
+{
+	printf("Po wymnożeniu macierzy 'A' przez wektor 'x' otzymujemy macierz o wymiarach (%dx%d):\n", wa, kx);
+	float suma;
+	for (int a = 0; a < wa; a++)
+	{
+		printf("[ ");
+		suma = 0;
+		for (int b = 0; b < ka; b++)
+		{			
+			suma = suma + (taba[a][b] * tabx[b][0]);						
+		}
+		printf("%.1f", suma);
+		printf(" ]\n");
+		
+	}
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	FILE* fina;
+	int wa, ka;
+	float taba[100][100];
+	fina = fopen(argv[1], "r");
+	fscanf(fina, "%d%d", &wa, &ka);
+	macierz(fina, wa, ka, taba);
+
+	FILE* finx;
+	int wx, kx;
+	float tabx[100][100];
+	finx = fopen(argv[2], "r");
+	fscanf(finx, "%d%d", &wx, &kx);
+	wektor(finx, wx, kx, tabx);
+
+	wymnoz(finx, fina, taba, tabx, wa, ka, wx, kx);
+	
+	return 0;
+}
